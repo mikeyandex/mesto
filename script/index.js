5f6df668fd3782a78a5ad64092f6d36779bcde66
Michael Melnikov <mike0907@yandex.ru>
2022-12-12 21:25:42 +0300

    12.12

--- script/index.js
+++ script/index.js
@@ -1,34 +1,41 @@
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__button-close');
+const addButton = document.querySelector('.popup__button-save');
 const popupOn = document.querySelector('.popup');
 
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
+
 const formInput = document.querySelectorAll('.popup__form-input');
 
 //Открытие попапа
 editButton.addEventListener('click', () => {
   popupOn.classList.add('popup_opened');
-  });
-  
+  formInput[0].value = profileTitle.textContent;
+  formInput[1].value = profileSubtitle.textContent;
+});
+
 //Закрытие  попапа
 closeButton.addEventListener('click', () => {
   popupOn.classList.remove('popup_opened');
-  });
-  
-/*При открытии формы поля «Имя» и «О себе» должны быть заполнены теми значениями, которые отображаются на странице.*/  
-formInput[0].value = profileTitle.textContent;
-formInput[1].value = profileSubtitle.textContent;
+});
+
+//Сохранение изменений
+addButton.addEventListener('click', () => {
+  popupOn.classList.remove('popup_opened');
+  profileTitle.textContent = formInput[0].value;
+  profileSubtitle.textContent = formInput[1].value;
+});
 
 /*Отправка формы*/
-let formElement = document.querySelector('.popup__container');
+let formElement = document.querySelector('form');
 let nameInput = formInput[0];
 let jobInput = formInput[1];
 
-function handleFormSubmit (evt) {
-evt.preventDefault(); 
-profileTitle.textContent = nameInput.value;
-profileSubtitle.textContent = jobInput.value;
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
 }
 
 
