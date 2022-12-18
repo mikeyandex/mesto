d91251e0da684f175de8773787f631f02da5a90a
Michael Melnikov <mike0907@yandex.ru>
2022-12-18 20:40:39 +0300

    18.12

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,55 @@
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__button-close');
-const addButton = document.querySelector('.popup__button-save');
+const saveButton = document.querySelector('.popup__button-save');
 const popupOn = document.querySelector('.popup');
+
+const nameInput = document.querySelector('.popup__form-input_type_name'); 
+const jobInput = document.querySelector('.popup__form-input_type_job'); 
 
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
 const formInput = document.querySelectorAll('.popup__form-input');
 
-//Открытие попапа
-editButton.addEventListener('click', () => {
+function openPopup() {
   popupOn.classList.add('popup_opened');
-  formInput[0].value = profileTitle.textContent;
-  formInput[1].value = profileSubtitle.textContent;
-});
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}
+
+//Открытие попапа
+editButton.addEventListener('click', openPopup);
+
+function closePopup() {
+  popupOn.classList.remove('popup_opened');
+}
 
 //Закрытие  попапа
-closeButton.addEventListener('click', () => {
-  popupOn.classList.remove('popup_opened');
-});
+closeButton.addEventListener('click', closePopup);
+
+function saveChanges() {  
+  profileTitle.textContent = nameInput.value;
+  console.log(nameInput.value)
+  profileSubtitle.textContent = jobInput.value;
+  closePopup();
+}
 
 //Сохранение изменений
-addButton.addEventListener('click', () => {
-  popupOn.classList.remove('popup_opened');
-  profileTitle.textContent = formInput[0].value;
-  profileSubtitle.textContent = formInput[1].value;
-});
+
+saveButton.addEventListener('click', saveChanges);
+
 
 /*Отправка формы*/
+
 let formElement = document.querySelector('form');
-let nameInput = formInput[0];
-let jobInput = formInput[1];
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
+  closePopup()
 }
-
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit); 
+formElement.addEventListener('submit', handleFormSubmit);
