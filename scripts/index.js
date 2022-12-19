168d6824123f185219dbe1114a9353d9eb992921
Michael Melnikov <mike0907@yandex.ru>
2022-12-19 22:33:15 +0300

    19.12

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,5 @@
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__button-close');
-const saveButton = document.querySelector('.popup__button-save');
 const popupOn = document.querySelector('.popup');
 
 const nameInput = document.querySelector('.popup__form-input_type_name'); 
@@ -8,8 +7,6 @@ const jobInput = document.querySelector('.popup__form-input_type_job');
 
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-
-const formInput = document.querySelectorAll('.popup__form-input');
 
 function openPopup() {
   popupOn.classList.add('popup_opened');
@@ -27,21 +24,11 @@ function closePopup() {
 //Закрытие  попапа
 closeButton.addEventListener('click', closePopup);
 
-function saveChanges() {  
-  profileTitle.textContent = nameInput.value;
-  console.log(nameInput.value)
-  profileSubtitle.textContent = jobInput.value;
-  closePopup();
-}
-
-//Сохранение изменений
-
-saveButton.addEventListener('click', saveChanges);
 
 
 /*Отправка формы*/
 
-let formElement = document.querySelector('form');
+let formElement = document.querySelector('.popup__form-mesto');
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
